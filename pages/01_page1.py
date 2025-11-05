29dfbc9801c40c309273ad7f9d9c662f42fdc6ef
Jie-Yan094 <jarita0904@gmail.com>
2025-11-05 03:22:42 +0000

    modified:   pages/01_page1.py

--- pages/01_page1.py
+++ pages/01_page1.py
@@ -1,32 +1,21 @@
 import solara
 import leafmap.maplibregl as leafmap
 
-
 def create_map():
 
     m = leafmap.Map(
-        style="dark-matter",
-        projection="globe",
+        center=[121.55555, 25.08722],
+        zoom=16,
+        pitch=60,
+        bearing=-17,
+        style="positron",
         height="750px",
-        zoom=2.5,
         sidebar_visible=True,
     )
+    m.add_basemap("CartoDB.DarkMatter")
+    m.add_geojson("https://bdon.github.io/taipei_mrt/", layer_name="w")
 
-    m.add_ee_layer(asset_id="ESA/WorldCover/v200", opacity=0.8)
-    m.add_overture_3d_buildings()
-
-    m.add_legend_to_sidebar(
-        builtin_legend="ESA_WorldCover", title="Land Cover Type", shape_type="rectangle"
-    )
-    m.add_colorbar_to_sidebar(cmap="terrain", label="Elevation")
-
-    image = "https://i.imgur.com/KeiAsTv.gif"
-    m.add_image_to_sidebar(image=image, expanded=False)
-
-    video = "https://static-assets.mapbox.com/mapbox-gl-js/drone.mp4"
-    m.add_video_to_sidebar(video, expanded=False)
     return m
-
 
 @solara.component
 def Page():
